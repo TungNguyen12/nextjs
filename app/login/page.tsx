8735844668cbca4b9c852399cc63c157efbf9dd4
TungNguyen12 <nvtung382@gmail.com>
2024-03-22 23:21:49 +0200

    adding authentication with next

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,18 @@
+import AcmeLogo from '../ui/acme-logo'
+import LoginForm from '../ui/login-form'
+
 export default function Login() {
-    return <p>Login</p>
+  return (
+    <main>
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
+          </div>
+
+          <LoginForm />
+        </div>
+      </div>
+    </main>
+  )
 }
